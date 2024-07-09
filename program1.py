6a03dcbe6cfb6e853cce3801c2c7aea953abcb26
chethankumar <124861594+chathankumar@users.noreply.github.com>
2024-07-09 13:01:36 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,18 +1,11 @@
-# Write to a file
 with open('example.txt', 'w') as file:
     file.write("Hello, World!\nThis is a text file example.\n")
-
-# Read from a file
 with open('example.txt', 'r') as file:
     content = file.read()
     print("File content:")
     print(content)
-
-# Update the file
 with open('example.txt', 'a') as file:
     file.write("Appending new content to the file.\n")
-
-# Read the updated file
 with open('example.txt', 'r') as file:
     updated_content = file.read()
     print("\nUpdated file content:")
